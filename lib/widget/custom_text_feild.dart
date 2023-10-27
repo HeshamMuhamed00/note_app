24fb98f71c8d95e19a3fb1d5c9dddfac10443432
Hesham Mohamed <heshammuh@gmail.com>
2023-10-27 22:57:24 +0200

    create custom text feild

---
+++ lib/widget/custom_text_feild.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widget/constants.dart';
+
+class CustomTextFeild extends StatelessWidget {
+  const CustomTextFeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Titel',
+        hintStyle: const TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
+  }
+}
