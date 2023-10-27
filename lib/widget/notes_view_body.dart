a3afcd03e86f578f3a909dded13f71adaa8298af
Hesham Mohamed <heshammuh@gmail.com>
2023-10-27 16:29:24 +0200

    show model bottom sheet

--- lib/widget/notes_view_body.dart
+++ lib/widget/notes_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widget/notes_list_view.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -23,19 +23,5 @@ class NotesViewBody extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class NotesListView extends StatelessWidget {
-  const NotesListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return const Padding(
-        padding: EdgeInsets.symmetric(vertical: 8),
-        child: NoteItem(),
-      );
-    });
   }
 }
