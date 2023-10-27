de0fca6978240f3d1d15b2abd143f75497c4083c
Hesham Mohamed <heshammuh@gmail.com>
2023-10-27 03:16:05 +0200

    create notes list view

---
+++ lib/widget/custom_note_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16), color: Colors.green),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        ListTile(
+          title: const Padding(
+            padding: EdgeInsets.only(bottom: 16),
+            child: Text(
+              'Flutter Tips',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          subtitle: const Padding(
+            padding: EdgeInsets.only(bottom: 16),
+            child: Text(
+              'Build Your Career With Hesham Mohamed',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                size: 32,
+              )),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(right: 24),
+          child: Text(
+            'May 21,2023',
+            style: TextStyle(fontSize: 16),
+          ),
+        )
+      ]),
+    );
+  }
+}
