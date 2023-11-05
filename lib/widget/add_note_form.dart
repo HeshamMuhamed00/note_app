5e3eaf149ae6ab7b79acf00f03caa6af288f3709
Hesham Mohamed <heshammuh@gmail.com>
2023-11-05 16:18:21 +0200

    delete note is done

--- lib/widget/add_note_form.dart
+++ lib/widget/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:note_app/cubits/add_note_cubit/add_notes_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 
@@ -57,10 +58,15 @@ class _AddNoteFormState extends State<AddNoteForm> {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
 
+                    var currentDate = DateTime.now();
+
+                    var formattedCurrentDate =
+                        DateFormat.yMd().format(currentDate);
+
                     var noteModel = NoteModel(
                         title: title!,
                         subTitle: subTitle!,
-                        date: DateTime.now().toString(),
+                        date: formattedCurrentDate,
                         color: Colors.green.value);
                     BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
                   } else {
